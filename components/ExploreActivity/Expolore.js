1f42ab1c1460dc6dfdfc5019ec7393182b3f29b8
Nik0di3m <norbert.waleszczyk@icloud.com>
2021-08-14 18:49:12 +0200

    deplot fix

--- components/ExploreActivity/Expolore.js
+++ components/ExploreActivity/Expolore.js
@@ -6,7 +6,7 @@ const Expolore = ({ data }) => {
             <h2 className="text-4xl font-semibold py-8">Odkryj spospoby spędzania czasu</h2>
             <div className="flex space-x-8 lg:justify-between overflow-x-scroll lg:overflow-x-hidden p-4 pb-4 -ml-2 scrollbar scrollbar-thin">
                 {data?.map((item) => (
-                    <BigCard image={item.Image} desc={item.Text} title={item.Title} />
+                    <BigCard key={item.id} image={item.Image} desc={item.Text} title={item.Title} />
                 ))}
             </div>
         </section>
--- components/LiveAnyWhere/LiveAnyWhere.js
+++ components/LiveAnyWhere/LiveAnyWhere.js
@@ -8,7 +8,7 @@ const LiveAnyWhere = ({ data }) => {
             <h2 className="text-4xl font-semibold py-8">Zamieszkaj w dowolnym miejscu</h2>
             <div className="flex space-x-4 overflow-x-visible overflow-y-hidden p-2 pb-4 -ml-2 scrollbar scrollbar-thin">
                 {data?.map((item) => (
-                    <LiveCard image={item.Image} text={item.Text} />
+                    <LiveCard key={item.id} image={item.Image} text={item.Text} />
                 ))}
             </div>
         </section>
